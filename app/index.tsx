91c31c856ac1b011e982d8c39fb35b4a87ce2109
redcliff-op <devyashjangid@gmail.com>
2024-10-30 19:19:08 +0530

    TreeWide: Init NativeWind and Cleanup tree

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const index = () => {
+  return (
+    <SafeAreaView className='flex-1 bg-zinc-900'>
+      
+    </SafeAreaView>
+  )
+}
+
+export default index

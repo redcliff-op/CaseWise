1814ef2099e9c9247424e2ae27f1607c2c31f25e
redcliff-op <devyashjangid@gmail.com>
2024-10-31 10:37:59 +0530

    EdgeToEdge: Switch to background color for navbar and statusbar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,7 @@ import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
 import "../global.css";
+import * as NavigationBar from 'expo-navigation-bar';
 
 SplashScreen.preventAutoHideAsync();
 
@@ -22,8 +23,10 @@ export default function RootLayout() {
     return null;
   }
 
+  NavigationBar.setBackgroundColorAsync("#F4EEE4")
+
   return (
-    <Stack screenOptions={{headerShown:false, statusBarStyle: 'dark'}}>
+    <Stack screenOptions={{ headerShown: false, statusBarStyle: 'dark', statusBarColor:'#F4EEE4'}}>
       <Stack.Screen name="index" />
       <Stack.Screen name="Home" options={{animation:'fade_from_bottom'}} />
     </Stack>

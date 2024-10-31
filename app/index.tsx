3cafe41bf6a1fd6357fb0ddedb3341d817c815f2
redcliff-op <devyashjangid@gmail.com>
2024-10-31 23:37:02 +0530

    AuthScreen: Add lottie animation

--- app/index.tsx
+++ app/index.tsx
@@ -1,28 +1,41 @@
-import { View, Text, Pressable } from 'react-native'
-import React, { useEffect } from 'react'
+import { View, Text, Pressable, Dimensions } from 'react-native'
+import React, { useEffect, useRef } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import useStore from '@/store/useStore'
 import { useShallow } from 'zustand/shallow'
+import LottieView from 'lottie-react-native'
 
 const index = () => {
 
   const [signIn, signInSilent] = useStore(
-    useShallow((state)=>[state.signIn, state.signInSilent])
+    useShallow((state) => [state.signIn, state.signInSilent])
   )
 
   useEffect(() => {
     signInSilent()
   }, [])
 
+  const animation = useRef(null)
+
   return (
     <SafeAreaView className='flex-1 justify-between bg-background items-center px-4'>
-      <View className='justify-center items-center flex-1'>
+      <View className='items-center flex-1 justify-center'>
         <Text className='text-black font-thin text-6xl'>
           CaseWise!
         </Text>
         <Text className='text-black text-lg'>
           Redefining Legal Clarity with AI!
         </Text>
+        <LottieView
+          autoPlay
+          loop
+          source={require('../assets/raw/hammer.json')}
+          ref={animation}
+          style={{
+            width: Dimensions.get('screen').width *0.7,
+            height: Dimensions.get('screen').height * 0.3,
+          }}
+        />
       </View>
       <Pressable
         onPress={signIn}
